06d8a19c0a7bbb12986312935841bae99b6b7c83
Chirag Singh <77009603+Chirag-Sngh@users.noreply.github.com>
2023-01-11 11:03:11 +0530

    Time: 2 ms (87.83%), Space: 42.7 MB (68.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -15,16 +15,28 @@ class Solution {
 //     }
 // }
 
-int[] ans=new int[2];
-for(int i=0;i<nums.length-1;++i){
-    for(int j=i+1;j<nums.length;++j){
-        if(nums[i]+nums[j]==target){
-            ans[0]=i;
-            ans[1]=j;
-            break;
+// int[] ans=new int[2];
+// for(int i=0;i<nums.length-1;++i){
+//     for(int j=i+1;j<nums.length;++j){
+//         if(nums[i]+nums[j]==target){
+//             ans[0]=i;
+//             ans[1]=j;
+//             break;
+//         }
+//     }
+// }
+// return ans;
+// }
+// }
+        
+    Map<Integer, Integer> indexMap=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            Integer requiredNum=target-nums[i];
+            if(indexMap.containsKey(requiredNum)){
+                return new int[]{indexMap.get(requiredNum),i};
+            }
+            indexMap.put(nums[i],i);
         }
+        return new int[] {-1,-1};
     }
-}
-return ans;
-}
-}
+}    
