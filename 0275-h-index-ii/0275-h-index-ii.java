45af98e9f2fd40f0ed6927ff418d710a802ca1c7
Chirag Singh <77009603+Chirag-Sngh@users.noreply.github.com>
2023-04-11 10:26:40 +0530

    Time: 0 ms (100.00%), Space: 49.6 MB (60.60%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,11 +1,27 @@
 class Solution {
     public int hIndex(int[] citations) {
+        // int n=citations.length;
+        // for(int i=0;i<n;i++){
+        //     if(n-i<=citations[i]){
+        //         return n-i;
+        //     }
+        // }
+        // return 0;
+        
+        int ans=0;
         int n=citations.length;
-        for(int i=0;i<n;i++){
-            if(n-i<=citations[i]){
-                return n-i;
+        int start=0,end=n;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(n-mid<=citations[mid]){
+                ans=Math.max(n-mid,ans);
+                end=mid;
+            }
+            else{
+                start=mid+1;
             }
         }
-        return 0;
+        return ans;
+        
     }
 }
